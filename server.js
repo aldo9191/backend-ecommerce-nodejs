1f7984728e48eefd40ae1321e7186a6b6e0ca158
aldo9191 <aldocanales91@gmail.com>
2022-02-10 10:14:51 -0500

    Adding nodemon dependencies

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
+// routes
+
+app.get('/', (req,res) => {
+  res.send('Welcome to Aldos Api')
+})
+
+// server running
+
 app.listen(app.get('port'), ()=> {
   console.log('Server is running on port', app.get('port'))
 })
