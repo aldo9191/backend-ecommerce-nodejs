2ffbc76ffec69a8b36d8a9d98f9a185c413b04c3
aldo9191 <aldocanales91@gmail.com>
2022-02-10 19:04:36 -0500

    adding last function to search product by name

--- routes.js
+++ routes.js
@@ -31,6 +31,39 @@ routes.get('/categorias', (req,res) => {
   })
 })
 
+// Obteniendo los productos segun la categoria
+
+routes.get('/productosporcategoria', (req,res)=>{
+  req.getConnection((err,conn) => {
+
+    if (err) return res.send(err)
+
+    const idCategory = req.query.idCategory
+
+    conn.query(`SELECT * FROM product WHERE category=${idCategory}`, (err, productsbyCategory) =>{
+      if (err) return res.send(err)
+
+      res.json(productsbyCategory)
+    })
+  })
+})
+
+// Obteniendo los productos por nombre
+
+routes.get('/productospornombre', (req,res) => {
+  req.getConnection((err,conn) =>{
+
+    if (err) return res.send(err)
+
+    const name = req.query.name
+
+    conn.query(`SELECT * FROM product where name like %${name}`, (err, productbyName) => {
+      if (err) return res.send(err)
+
+      res.json(productbyName)
+    })
+  })
+})
 
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -14,6 +14,14 @@ const dbOptions = {
   database:'bsale_test'
 }
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
+
 // middlewares
 
 app.use(myconn(mysql, dbOptions, 'single'))
